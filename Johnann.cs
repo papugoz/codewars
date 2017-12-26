225f51726acb1887ef3fe308eac77c300c6e1b0f
Marcin Ciesla <papugoz@gmail.com>
2017-12-26 14:42:22 +0100

    johnann

---
+++ Johnann.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+public static class Johnann
+{
+    public static List<long> John(long n)
+    {
+        var johnKatas = new List<long> { 0 };
+        for (int i = 1; i < n; i++)
+        {
+            
+        }
+        return johnKatas;
+    }
+    public static List<long> Ann(long n)
+    {
+        // your code
+    }
+    public static long SumJohn(long n)
+    {
+        // your code
+    }
+    public static long SumAnn(long n)
+    {
+        // your code
+    }
+}
